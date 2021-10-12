eea18ef294679f5869c689fa601c55ec68866f9c
Bogdan <bogdanemails@gmail.com>
2021-10-12 13:09:25 +0300

    Updates

---
+++ precache-manifest.9d1454983a397cd9b0507510fe5a8a5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9fb943a37d63c977a16e88c7a87033",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba19a104d1274dfe5db7",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba19a104d1274dfe5db7",
+    "url": "./static/js/main.40e67025.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
